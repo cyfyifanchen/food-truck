db9423541ddf8638f687b9f4aa967b45a26a0c63
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-10-24 16:08:26 +0800

    Change the name to Web App instead

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ const App: React.FC = () => {
   return (
     <div className="w-full h-full text-center p-10">
       <h1 className="text-3xl font-bold mb-4 text-gray-800">
-        Food Truck Visualization
+        Food Truck Web App
       </h1>
       <div className="food-truck-list grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {foodTrucks.map((truck) => (
