d80c59ef48cda825d729bad1800296297021357c
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-12-15 19:34:23 +0800

    Changing font

--- app/ui/nav-links.tsx
+++ app/ui/nav-links.tsx
@@ -27,7 +27,7 @@ export default function NavLinks() {
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-fresh-cardColor p-3 text-sm font-medium hover:bg-fresh-subTextColor hover:text-white md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-fresh-subTextColor text-white': pathname === link.href,
+                'bg-fresh-textColor text-white': pathname === link.href,
               }
             )}
           >
